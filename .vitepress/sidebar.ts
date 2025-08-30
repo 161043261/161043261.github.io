15a9357605e335981570b04b5981d72e38fa005c
hangtiancheng <hangtiancheng@bytedance.com>
2025-08-30 20:01:49 +0800

    Initial commit

---
+++ .vitepress/sidebar.ts
@@ -0,0 +1,45 @@
+export default {
+  "/base/": [
+    {
+      items: [
+        { text: "C++", link: "/base/languages/cpp" },
+        { text: "JS/TS", link: "/base/languages/esnext" },
+        { text: "Go", link: "/base/languages/go" },
+        { text: "Python", link: "/base/languages/python" },
+        { text: "Algorithm", link: "/base/algorithm" },
+        { text: "Git", link: "/base/git" },
+        { text: "Linux", link: "/base/linux" },
+        { text: "Network", link: "/base/network" },
+        { text: "Network(2)", link: "/base/network2" },
+      ],
+    },
+  ],
+  "/frontend/": [
+    {
+      items: [
+        { text: "Bundler", link: "/frontend/bundler" },
+        { text: "CSS", link: "/frontend/css" },
+        { text: "CSS(2)", link: "/frontend/css2" },
+        { text: "Vue", link: "/frontend/vue" },
+        { text: "Vue.pro", link: "/frontend/vue-pro" },
+        { text: "Vue Router", link: "/frontend/vue-router" },
+        { text: "Pinia", link: "/frontend/pinia" },
+        { text: "React", link: "/frontend/react" },
+        { text: "React Router", link: "/frontend/react-router" },
+        { text: "Zustand", link: "/frontend/zustand" },
+      ],
+    },
+  ],
+  "/backend/": [
+    {
+      items: [
+        { text: "Fiber", link: "/backend/fiber" },
+        { text: "Kafka", link: "/backend/kafka" },
+        { text: "MongoDB", link: "/backend/mongodb" },
+        { text: "MySQL", link: "/backend/mysql" },
+        { text: "Nest.js", link: "/backend/nest" },
+        { text: "Redis", link: "/backend/redis" },
+      ],
+    },
+  ],
+};
---
+++ src/index.md
@@ -0,0 +1,32 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+hero:
+  name: 未
+  text: 来
+  tagline: へ lovelove
+  image:
+    src: /index.jpg
+    alt: index
+  actions:
+    - theme: brand
+      text: Network
+      link: /base/network2
+    - theme: brand
+      text: Algorithm
+      link: /base/algorithm
+    - theme: alt
+      text: React
+      link: /frontend/react
+    - theme: alt
+      text: Vue
+      link: /frontend/vue
+
+features:
+  - title: Base
+    details: JS/TS, Python, Go, C++, Linux, Network, Git
+  - title: Frontend
+    details: HTML/CSS, npm/pnpm, Vue, vue-router, Pinia, React, react-router, Zustand, Webpack, Vite
+  - title: Backend
+    details: Express.js, Nest.js, Fiber, MongoDB, MySQL, Redis, Kafka, Protobuf
+---
